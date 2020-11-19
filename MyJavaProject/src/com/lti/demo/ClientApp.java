9bd4bc5ee08b1cf7c9ea160e8b9db01acc9f1402
projectmaster98 <tpgame42@gmail.com>
2020-11-19 17:41:39 +0530

    My First Commit Java Project

---
+++ MyJavaProject/src/com/lti/demo/ClientApp.java
@@ -0,0 +1,11 @@
+package com.lti.demo;
+
+public class ClientApp {
+    
+    public static void main(String[] args)
+    {
+        Person p = new Person("Jay",101,90.00);
+        System.out.println(p);
+        
+    }
+}
